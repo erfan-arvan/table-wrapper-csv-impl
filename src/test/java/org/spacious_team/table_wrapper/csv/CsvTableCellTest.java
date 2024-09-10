73616b075afe3d5659ce77cf670f9a5f0e612052
Erfan Arvan <ea442@njit.edu>
2024-09-10 00:15:45 -0400

    removed commands, annos, and imports

--- src/test/java/org/spacious_team/table_wrapper/csv/CsvTableCellTest.java
+++ src/test/java/org/spacious_team/table_wrapper/csv/CsvTableCellTest.java
@@ -1,30 +1,5 @@
-/*
- * Table Wrapper CSV Impl
- * Copyright (C) 2022  Spacious Team <spacious-team@ya.ru>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package org.spacious_team.table_wrapper.csv;
-
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class CsvTableCellTest {
-
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5})
     void getColumnIndex(int colNum) {
--- src/test/java/org/spacious_team/table_wrapper/csv/CsvTableRowTest.java
+++ src/test/java/org/spacious_team/table_wrapper/csv/CsvTableRowTest.java
@@ -1,34 +1,5 @@
-/*
- * Table Wrapper CSV Impl
- * Copyright (C) 2022  Spacious Team <spacious-team@ya.ru>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package org.spacious_team.table_wrapper.csv;
-
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-
 class CsvTableRowTest {
-
     @ParameterizedTest
     @MethodSource("indexAndRow")
     void getCell(int rowNum, String[] row, int colNum) {
@@ -41,7 +12,6 @@ class CsvTableRowTest {
             assertEquals(cell, csvRow.getCell(colNum));
         }
     }
-
     @ParameterizedTest
     @MethodSource("indexAndRow")
     void getFirstCellNum(int rowNum, String[] row) {
@@ -52,14 +22,12 @@ class CsvTableRowTest {
             assertEquals(0, csv.getFirstCellNum());
         }
     }
-
     @ParameterizedTest
     @MethodSource("indexAndRow")
     void getLastCellNum(int rowNum, String[] row) {
         CsvTableRow csv = new CsvTableRow(row, rowNum);
         assertEquals(row.length - 1, csv.getLastCellNum());
     }
-
     private static Stream<Arguments> indexAndRow() {
         String[] row = new String[]{"1", "2.1", "abc", "2020-01-01 01:02:03"};
         return Stream.of(
